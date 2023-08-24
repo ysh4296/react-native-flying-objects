52ba456b052ec1274a4741e2c67d0a4638076041
cadi <dbtmdgns4296@naver.com>
2023-08-24 14:38:31 +0900

    readme 추가, 데모 코드 간소화

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,41 +1,10 @@
-import React, { useMemo, useState } from 'react';
-import {Text,StyleSheet, SafeAreaView, Pressable, Easing} from 'react-native';
+import React, { useState } from 'react';
+import {Text,StyleSheet, SafeAreaView, Pressable} from 'react-native';
 
 import FlyingView from 'react-native-flying-objects';
 
 const App = () => {
   const [object, setObject] = useState<ObjectConfig[]>([]);
-  const objectConfig = useMemo(
-    () => ({
-      right: {
-        fromValue: 15,
-        toValue: Math.floor(Math.random() * 30),
-        duration: 1200,
-        easing:Easing.bezier(
-          Math.floor(Math.random() * 10) / 10,
-          Math.floor(Math.random() * 10) / 10 + 0.7,
-          Math.floor(Math.random() * 10) / 10,
-          Math.floor(Math.random() * 10) / 10 + 0.7,
-        ),
-      },
-      top: {
-        fromValue: 100,
-        toValue: 35,
-        duration: 1200,
-        easing: Easing.bezier(0.5, 1.0, 0.5, 1.0),
-      },
-      show: {
-        fromValue: 0,
-        toValue: 1,
-      },
-      hide:{
-        fromValue: 1,
-        toValue: 0,
-      },
-    }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [object]
-  );
 
   return (
     <SafeAreaView style={styles.container}>
@@ -44,7 +13,6 @@ const App = () => {
         </FlyingView>
         <Pressable
           onPress={() => {
-            console.log(objectConfig);
             setObject((prev) => [...prev,{}]);
           }}
           style={{ backgroundColor : 'yellow' }}
